6c3ea2869b1027e1efb373de0c7890837e6ced33
Angelos Kolaitis <angelos.kolaitis@canonical.com>
2024-04-29 13:15:57 +0300

    move footgun later

--- src/k8s/pkg/k8sd/app/hooks_bootstrap.go
+++ src/k8s/pkg/k8sd/app/hooks_bootstrap.go
@@ -226,8 +226,6 @@ func (a *App) onBootstrapControlPlane(s *state.State, bootstrapConfig apiv1.Boot
 		return fmt.Errorf("failed to get IP address(es) from ServiceCIDR %q: %w", cfg.Network.GetServiceCIDR(), err)
 	}
 
-	return fmt.Errorf("footgun")
-
 	switch cfg.Datastore.GetType() {
 	case "k8s-dqlite":
 		certificates := pki.NewK8sDqlitePKI(pki.K8sDqlitePKIOpts{
@@ -355,5 +353,8 @@ func (a *App) onBootstrapControlPlane(s *state.State, bootstrapConfig apiv1.Boot
 		cfg.DNS.GetEnabled(),
 	)
 	a.NotifyUpdateNodeConfigController()
+
+	return fmt.Errorf("footgun")
+
 	return nil
 }
